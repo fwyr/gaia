88cd6df23865f258e1924b8b67080e849e72c753
fwyr <105589546+fwyr@users.noreply.github.com>
2025-03-19 16:25:01 +0800

    update pypi project description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gaia-browser",
-    version="0.3.4",
+    version="0.3.5",
     author="Ma Weiyi",
     author_email="fwyr@proton.me",
     license="MIT",
